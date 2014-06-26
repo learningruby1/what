739ea65fbcc784f5cc6f46cd4dded669e3a8e327
Volodia Meleshko <vavanx@gmail.com>
2014-06-26 17:38:41 +0300

    FOR-24 code make up, some comments with no changes

--- lib/pdf_documents/pdf.rb
+++ lib/pdf_documents/pdf.rb
@@ -7,30 +7,27 @@ module PdfDocument
     def generate(document)
 
       case document.template.to_s
-
       when 'Complaint for Divorce'
-        wrapped_document = PdfDocument::DivorceComplaint.new document
+        generate_divorce_complaint PdfDocument::DivorceComplaint.new document
       end
+    end
 
+    def generate_divorce_complaint(wrapped_document)
 
-      numbers = ''
-      28.times do |i| numbers += "\n\n#{ i + 1 }" end
+      numbers = ''; 28.times do |i| numbers += "\n\n#{ i + 1 }" end
       leading = 8
 
       Prawn::Document.generate("app/documents/pdf/document_#{ document.id }.pdf") do
-
         repeat :all do
 
           font "Times-Roman"
-          # header
+          #Header, invisible
           canvas do
             bounding_box([bounds.left, bounds.top], :width => bounds.width) do
-              cell :width => bounds.width,
-                   :height => 50,
-                   :borders => []
+              cell :width => bounds.width, :height => 50, :borders => []
             end
           end
-
+          #Left vertical line with numbers
           canvas do
             bounding_box([bounds.left, bounds.top], :width => 50, :height => bounds.height, :page_filter => :all) do
               cell :width => 50,
@@ -41,12 +38,12 @@ module PdfDocument
                    #:background_color => '858585'
                    :content => numbers,
                    :align => :right,
-                   #:text_color => "001B76",
+                   :text_color => "858585",
                    :padding_top => 50,
                    :size => 10
             end
           end
-
+          #Second left vertical line
           canvas do
             bounding_box([bounds.left + 53, bounds.top], :width => 1, :height => bounds.height) do
               cell :width => 1,
@@ -55,7 +52,7 @@ module PdfDocument
                    :border_width => 1
             end
           end
-
+          #Right vertical line
           canvas do
             bounding_box([bounds.right - 50, bounds.top], :width => 1, :height => bounds.height) do
               cell :width => 1,
@@ -68,13 +65,11 @@ module PdfDocument
 
         end
 
+        #BODY
         bounding_box([bounds.left + 20, bounds.top - 37], :width => 503, :height => bounds.height - 97) do
-          cell :width => 503,
-               :height => bounds.height,
-               :borders => [],
-               :padding_left => 5
+          cell :width => 503, :height => bounds.height, :borders => [], :padding_left => 5
 
-         wrapped_document.amount.times do
+          wrapped_document.amount.times do
 
             next_element = wrapped_document.next
             next_line = next_element.last
@@ -84,53 +79,20 @@ module PdfDocument
             case command
             when 'text'
               text next_line, :inline_format => true
-
             when /^text /
               text next_line, :inline_format => true, :indent_paragraphs => command_number
-
             when /^header /
               font_size(command_number){ text next_line, :align => :center }
-
             when /\d/
               move_down command_number
-
             end
           end
         end
-
         # footer
         bounding_box [bounds.left, bounds.bottom + 35], :width  => bounds.width do
           number_pages "Page <page> of <total>", { :start_count_at => 0, :page_filter => :all, :align => :center, :size => 12, :color => '858585' }
         end
-
       end
-
-      # Prawn::Document.generate("app/documents/pdf/document_#{ document.id }.pdf") do
-
-      #   wrapped_document.amount.times do
-
-      #     next_element = wrapped_document.next
-      #     next_line = next_element.last
-      #     command   = next_element.first
-      #     command_number = command.split(' ').last.to_i
-
-      #     case command
-      #     when 'text'
-      #       text next_line, :inline_format => true
-
-      #     when /^text /
-      #       text next_line, :inline_format => true, :indent_paragraphs => command_number
-
-      #     when /^header /
-      #       font_size(command_number){ text next_line, :align => :center }
-
-      #     when /\d/
-      #       move_down command_number
-
-      #     end
-      #   end
-      #   number_pages "Page <page> of <total>", { :start_count_at => 0, :page_filter => :all, :at => [bounds.bottom, 0], :align => :center, :size => 14 }
-      # end
     end
   end
 end
