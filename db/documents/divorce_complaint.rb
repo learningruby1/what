cfc8d62387799e8c7bbc41ab80a4d824c28d98ce
leo <eugene.mogylskyy@teamvoy.com>
2014-07-18 11:30:29 +0300

    [FOR-68] Not mandatory questions

--- db/documents/divorce_complaint.rb
+++ db/documents/divorce_complaint.rb
@@ -39,10 +39,10 @@ current_step.fields.create :name => 'Middle Initial /<spain/>Inicial de Segundo 
 current_step.fields.create :name => 'Last Name /<spain/>Apellido: *'
 current_step.fields.create :name => 'Date of Birth /<spain/>Fecha de nacimiento: *', :field_type => 'date'
 current_step.fields.create :name => 'Social Security # /<spain/># Seguro Social:', :mandatory => false
-current_step.fields.create :name => 'Mailing Address or last Known address /<spain/>Dirección postal o última dirección : *'
-current_step.fields.create :name => 'City /<spain/>Ciudad: *'
-current_step.fields.create :name => 'State /<spain/>Estado: *'
-current_step.fields.create :name => 'Zip Code /<spain/>Código postal: *'
+current_step.fields.create :name => 'Mailing Address or last Known address /<spain/>Dirección postal o última dirección:'
+current_step.fields.create :name => 'City /<spain/>Ciudad:'
+current_step.fields.create :name => 'State /<spain/>Estado:'
+current_step.fields.create :name => 'Zip Code /<spain/>Código postal:'
 current_step.fields.create :name => 'Email /<spain/>Correo Electrónico:'
 current_step.fields.create :name => 'Phone number /<spain/>Número de teléfono:', :mandatory => false
 
