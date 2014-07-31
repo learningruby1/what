e8d3ca55fc741eaea8a754b8f4c91c50e7bdffc4
Volodia Meleshko <vavanx@gmail.com>
2014-07-31 12:37:33 +0300

    [FOR-86] add returning client fields

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,36 +1,9 @@
 class DocumentsController < ApplicationController
   skip_filter :authenticate_user!
-  before_action :get_template, :only => [:new, :create]
-  before_action :get_document, :only => [:create]
-
-  def new
-    @document = @template.documents.build
-  end
 
   def create
-    if @document.save
-      redirect_to document_answer_path(@document, 1)
-    else
-      render :action => 'new'
-    end
-  end
-
-  private
-  def get_template
-    @template = Template.find params[:template_id]
-  end
-
-  def document_params
-    params.require(:document).permit :title
-  end
-
-  def get_document
-    @document = @template.documents.build document_params
-    if user_signed_in?
-      @document.user_id = current_user.id
-    else
-      @document.session_uniq_token = cookies[:session_uniq_token].present? ? cookies[:session_uniq_token] : @document.generate_session_uniq_token
-      cookies[:session_uniq_token] = @document.session_uniq_token
-    end
+    document = Template.find(params[:template_id]).documents.build
+    document.assign_owner_save! cookies, (current_user if user_signed_in?)
+    redirect_to document_answer_path(document.id, 1)
   end
 end
