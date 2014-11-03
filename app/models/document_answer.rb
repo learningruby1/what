5a1851dfdcaa3692aa76cabf785586151c6a611e
Volodia Meleshko <vavanx@gmail.com>
2014-11-03 16:40:25 +0200

    [FOR-87] Spousal support: radio-text fix

--- app/models/document_answer.rb
+++ app/models/document_answer.rb
@@ -109,7 +109,11 @@ class DocumentAnswer < ActiveRecord::Base
   def to_spain
     return to_s if !template_field.field_type.match(/radio/) || template_field.field_type.match(/text_radio/)
     template_field.to_text(document).split('<option/>').each do |a|
-      if a.match(Regexp.new answer)
+      p '-'*100
+      p a
+      p answer
+      p '-'*100
+      if a.match(Regexp.new answer.split('(').first)
         if a.match '<spain/>'
           splited_answer = a.split '<spain/>'
           a = splited_answer[0] + ' <span class="spain">' + splited_answer[1] + '</span>'
