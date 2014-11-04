43f845fd2f98249ef8da21ca3e2135dbf69f79bc
leo <eugene.mogylskyy@teamvoy.com>
2014-11-04 16:55:37 +0200

    [FOR-255] Update code for review. Fieled case

--- app/models/template_field.rb
+++ app/models/template_field.rb
@@ -18,6 +18,12 @@ class TemplateField < ActiveRecord::Base
         name.gsub! '<spain_self>', get_self(document) == 'Wife' ? 'esposo' : 'esposa'
         name.gsub! '<uppercase_spain_self>', get_self(document) ? 'ESPOSO' : 'ESPOSA'
         name[0] = 'C' if name[0] == 'c'
+      elsif document.to_s == Document::FILED_CASE
+        if name.match(/<defendant_full_name>/) || name.match(/<defendant_full_name_spain>/)
+          defendant_name = get_defendant_full_name(document)
+          name.gsub!('<defendant_full_name>', defendant_name)
+          name.gsub!('<defendant_full_name_spain>', defendant_name)
+        end
       end
     end
     name
