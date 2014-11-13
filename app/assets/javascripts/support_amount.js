6e3133fc7b00e46b643c7bda33468bccd727c2a0
leo <eugene.mogylskyy@teamvoy.com>
2014-11-13 17:08:32 +0200

    [FOR-272] Update step 23, delete AJAX

--- app/assets/javascripts/support_amount.js
+++ app/assets/javascripts/support_amount.js
@@ -1,9 +1,9 @@
 $(function(){
   if($('.amount_block').length > 0){
     $('.amount_block:last').parent().parent().parent().append("<div class='container'><div class='row new_roman col-md-9'><div class='who_pay_text'></div></div></div>");
-    SupportAmount.who_pay($('.amount_block:last .income-math'), $('.amount_block:first .income-math'));
+    SupportAmount.who_pay($('.amount_block:last .income-math'), $('.amount_block:first .income-math'), $('.who_pay_text'));
     $('.amount_block .income-math').keyup(function(){
-      SupportAmount.who_pay($('.amount_block:last .income-math'), $('.amount_block:first .income-math'));
+      SupportAmount.who_pay($('.amount_block:last .income-math'), $('.amount_block:first .income-math'), $('.who_pay_text'));
     });
   }
 });
@@ -20,17 +20,19 @@ var SupportAmount = {
     return result;
   },
 
-  who_pay: function(dad_field, mom_field){
+  who_pay: function(dad_field, mom_field, text_field){
     var dad_result = SupportAmount.joint_child_support_amount(dad_field);
     var mom_result = SupportAmount.joint_child_support_amount(mom_field);
+    var mom = $('.amount_block:first .income-math').data('plaintiff-name');
+    var dad = $('.amount_block:first .income-math').data('defendant-name');
     if(dad_result == undefined || mom_result == undefined){
       return;
     }
-    $.ajax({
-      type: "GET",
-      data: { dad_result: dad_result, mom_result: mom_result },
-      url: "/documents/"+$('#document_id').val()+"/step/"+$('#step_id').val()+"/child_suport_prompt"
-    });
-
+    if(dad_result > mom_result){
+      text_field.html('<span><b>'+ dad + ' $' + dad_result + ' – ' + mom + ' $' + mom_result + ' = $' + (Math.round((dad_result - mom_result)* 100) / 100) + '. ' + dad + ' has to pay ' + (Math.round((dad_result - mom_result)* 100) / 100) + '$ per month for child support to ' + mom + '. /</span> <span class="spain">' + dad + ' $' + dad_result + ' –  ' + mom + ' $' + mom_result + ' = $' + (Math.round((dad_result - mom_result)* 100) / 100) + ' es la responsabilidad de manutención de menor que ' + dad + ' tendrá que pagar a ' + mom + ' por mes.</span>');
+    }
+    else{
+      text_field.html('<span><b>' + mom + ' $' + mom_result + ' – ' + dad + ' $' + dad_result + ' = $' + (Math.round((mom_result - dad_result)* 100) / 100) + '. ' + mom + ' has to pay ' + (Math.round((mom_result - dad_result)* 100) / 100) + '$ per month for child support to ' + dad + '. /</span> <span class="spain">' + mom + ' $' + mom_result + ' –  ' + dad + ' $' + dad_result + ' = $' + (Math.round((mom_result - dad_result)* 100) / 100) + ' es la responsabilidad de manutención de menor que ' + mom + ' tendrá que pagar a ' + dad + ' por mes.</span>');
+    }
   }
 }
